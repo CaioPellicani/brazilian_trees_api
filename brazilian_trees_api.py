2c52e4fcc7e064b6e5ad3ff65afbaa8830f02eda
Caio Pellicani <caiopellicani@hotmail.com>
2022-01-12 15:20:23 -0300

    IMP: selfstarting db

--- brazilian_trees_api.py
+++ brazilian_trees_api.py
@@ -1,5 +1,10 @@
+from genericpath import exists
 from flask import Flask, json
 from routes.root_route import RootRoute
+from modules.db import create_db
+
+if( not exists( "database.db" ) ):
+    create_db()
 
 app = Flask(__name__)
 
