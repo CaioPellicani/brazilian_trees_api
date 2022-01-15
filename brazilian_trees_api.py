8e67923f265b6d65457e003ff8c3057ed49de563
Caio Pellicani <caiopellicani@hotmail.com>
2022-01-14 21:34:20 -0300

    REF: limpeza

--- brazilian_trees_api.py
+++ brazilian_trees_api.py
@@ -2,6 +2,7 @@ from crypt import methods
 from genericpath import exists
 from flask import Flask, request, json
 from routes.root_route import RootRoute
+from routes.trees import routeTrees
 from modules.db import create_db
 
 if( not exists( "database.db" ) ):
@@ -10,3 +11,13 @@ if( not exists( "database.db" ) ):
 app = Flask(__name__)
 
 app.register_blueprint(RootRoute, url_prefix='/root')
+app.register_blueprint(routeTrees, url_prefix='/trees')
+
+@app.route('/teste', methods=[ 'GET', 'POST' ] )
+def teste():
+    if ( request.method == 'GET' ):
+        return '{"teste":"get"}'
+    elif ( request.method == "POST" ):
+        data = json.loads( request.data )
+        print( data['valor'] )
+        return '{"teste":"post"}'
