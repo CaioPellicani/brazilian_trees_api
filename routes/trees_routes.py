41077f731c224641a79b9603b4ec5686e2e443a1
Caio Pellicani <caiopellicani@hotmail.com>
2022-05-24 19:17:13 -0300

    unstaged old mods

--- routes/trees_routes.py
+++ routes/trees_routes.py
@@ -1,33 +1,61 @@
 from crypt import methods
+from ctypes import sizeof
+import dataclasses
 from datetime import datetime
 from flask import Blueprint, jsonify, make_response, request, json
 from itsdangerous import json
-from modules.db import get_db_connection
+from models.class_tree import Tree
 from modules.trees_query import getTreesBy
 from modules.commons import error404
 
 routeTrees = Blueprint('trees', __name__,)
 
-def getMaker( data ):
-    result = [{"timestamp": datetime.now()}]
-    treeList = getTreesBy( data )
-    
-    if( len( treeList ) == 0 ):
-        return error404( data )
-
-    result.append(treeList)
-    return make_response( jsonify(result) )
-
-@routeTrees.route( '/', methods = [ 'GET', 'POST' ] )
+@routeTrees.route( '/', methods = [ 'GET', 'POST', 'PUT' ] )
 def trees_root():
     if( request.method == 'GET' ):
         return getMaker( json.loads( request.data ) )
 
     if( request.method == 'POST' ):
-        result = '{"route":"trees_root", "met":"POST" }'
+        data = json.loads( request.data )
+        if( type( data ) == list ):
+            result = Tree( data[0] )
+        else:
+            result = Tree( data )
+        try:
+            return jsonify( id = result.post() )
+        except ValueError as x:
+            return jsonify( error = f"{x.args[0]}" ) 
 
-    return result
+    if( request.method == 'PUT' ):
+        data = json.loads( request.data )
+        if( type( data ) == list ):
+            result = Tree( data[0] )
+        else:
+            result = Tree( data )
+        try:
+            return jsonify( id = result.put() )
+        except ValueError as x:
+            return jsonify( error = f"{x.args[0]}" )          
 
+
+def getMaker( data ):
+    result = {
+        "timestamp": datetime.now(), 
+        "data":[]
+    }
+    treeList = []
+    if( type( data ) == list ):
+        for i in data:          
+            result["data"].append( getTreesBy( i ) )
+    else:
+        result["data"].append( getTreesBy( data ) )
+    
+    if( len( result["data"] ) == 0 ):
+        return error404( data )
+
+    #result["data"].append(treeList)
+
+    return make_response( jsonify(result) )
 
 @routeTrees.route('/id/<data>' )
 def trees_id(data): 
@@ -36,6 +64,10 @@ def trees_id(data):
 @routeTrees.route('/scientific_name/<data>' )
 def trees_sci_name(data): 
     return getMaker( { "scientific_name": data } )
+
+@routeTrees.route('/popular_name/<data>' )
+def trees_pop_name(data): 
+    return 'TODO'
 
 @routeTrees.route('/ecological_class/<data>' )
 def trees_eco_class(data):   
